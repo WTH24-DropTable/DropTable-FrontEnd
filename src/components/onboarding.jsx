9ef32cd56f5129ec8a49ea94027a6b081e2d4027
jer123se12 <jerickseng06@gmail.com>
2024-12-21 18:34:55 +0800

    amongus

---
+++ src/components/onboarding.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Onboarding(){
+  return <>
+      <div className="flex justify-center items-center h-screen w-screen fixed top-0 left-0 bg-[#00000055] backdrop-blur z-10">
+        <div className="text-center font-bold text-xl"> Lets Get You Into Our<br/>Database</div>
+      </div>
+    </>
+
+}
