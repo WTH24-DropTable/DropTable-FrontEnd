c9dc614486272d6a3eeb5d8e700ce2be2e13b34f
Alfred-Kang <alfredkangjr@gmail.com>
2024-12-22 10:40:52 +0800

    feat: mobile responsiveness of lecturer pages

--- src/app/lecturer/home/page.tsx
+++ src/app/lecturer/home/page.tsx
@@ -51,62 +51,83 @@ export default function Home() {
 
   return (
     <>
-        <div className="min-h-screen bg-gray-800 text-white font-barlow p-6">
-          <div className="flex flex-row items-center">
-            <h1 className="text-5xl font-bold text-lblue my-8">Welcome, { user?.name } </h1>
-            <button
-              className="ml-auto bg-lblue text-white px-6 py-2 h-fit rounded-lg font-bold hover:bg-blue-500 transition"
-              onClick={() => setShowCreateClass(true)}
-            >
-              Create Class
-            </button>
-          </div>
-
-          <div className="flex mt-6">
-            <div className="w-1/4 mr-6">
-              <h2 className="text-3xl font-semibold text-lblue mb-4">Classes</h2>
-              <div className="space-y-4">
-                {classes?.map((item, index) => (
-                  <div
-                    className="bg-gray-600 p-8 rounded-xl cursor-pointer"
-                    key={index}
-                    onClick={() => setSelectedClassId(item.id)}
-                  >
-                    <p className="text-xl font-bold">{ item.className }</p>
-                    <p className="text-lg">{ item.name } - { item.duration } hrs</p>
-                  </div>
-                ))}
-              </div>
-            </div>
-
-            {/* Schedule Section */}
-            <div className="flex-1">
-              { !selectedClassId && <h2 className="flex justify-center items-center text-3xl font-semibold text-white mb-4 p-3 w-full rounded-lg h-full">Select a Class to View Schedule</h2> }
-              { selectedClassId && (
-                <div className="flex-1">
-                    <h2 className="text-3xl font-semibold text-lblue mb-4">
-                      { classes?.find((classItem) => classItem.id === selectedClassId)?.name } - { classes?.find((classItem) => classItem.id === selectedClassId)?.className }
-                    </h2>
-                  <div className="bg-gray-600 p-6 rounded-xl space-y-6">
-                    {classOccurances?.map((item, index) => (
-                      <div
-                        key={index} className="flex justify-between cursor-pointer"
-                        onClick={() => router.push(`/lecturer/classlist?classId=${selectedClassId}&dateTime=${item.dateTime}`)}
-                      >
-                        <div>
-                          <p className="text-xl text-gray-400">{formatDate(item.dateTime)}</p>
-                        </div>
-                        <p className={`text-xl font-bold ${(item.expectedAttendees - item.attendees.length > ((item.expectedAttendees - item.attendees.length) / 2) ? "text-red-500" : "text-green-500")}`}>{item.attendees.length}/{item.expectedAttendees}</p>
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              )}
-            </div>
-          </div>
+      <div className="min-h-screen bg-gray-800 text-white font-barlow p-6">
+        <div className="flex flex-wrap items-center">
+          <h1 className="text-3xl md:text-5xl font-bold text-lblue my-4 md:my-8">
+            Welcome, {user?.name}
+          </h1>
+          <button
+            className="ml-auto bg-lblue text-white px-4 md:px-6 py-4 rounded-lg md:w-1/6 w-full font-bold hover:bg-blue-500 transition"
+            onClick={() => setShowCreateClass(true)}
+          >
+            Create Class
+          </button>
         </div>
 
-        {showCreateClass && <CreateClass hideCreateClass={hideCreateClass} />}
+        <div className="flex flex-col md:flex-row mt-6">
+          {/* Classes Section */}
+          <div className="w-full md:w-1/4 md:mr-6 mb-6 md:mb-0">
+            <h2 className="text-2xl md:text-3xl font-semibold text-lblue mb-4">
+              Classes
+            </h2>
+            <div className="space-y-4">
+              {classes?.map((item, index) => (
+                <div
+                  className="bg-gray-600 p-4 md:p-8 rounded-xl cursor-pointer"
+                  key={index}
+                  onClick={() => setSelectedClassId(item.id)}
+                >
+                  <p className="text-lg md:text-xl font-bold">{item.className}</p>
+                  <p className="text-sm md:text-lg">
+                    {item.name} - {item.duration} hrs
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          {/* Schedule Section */}
+          <div className="flex-1">
+            {!selectedClassId && (
+              <h2 className="flex justify-center items-center text-xl md:text-3xl font-semibold text-white mb-4 p-3 w-full rounded-lg h-full">
+                Select a Class to View Schedule
+              </h2>
+            )}
+            {selectedClassId && (
+              <div className="flex-1">
+                <h2 className="text-2xl md:text-3xl font-semibold text-lblue mb-4">
+                  {classes?.find((classItem) => classItem.id === selectedClassId)?.name} -{" "}
+                  {classes?.find((classItem) => classItem.id === selectedClassId)?.className}
+                </h2>
+                <div className="bg-gray-600 p-4 md:p-6 rounded-xl space-y-6">
+                  {classOccurances?.map((item, index) => (
+                    <div
+                      key={index}
+                      className="flex justify-between cursor-pointer"
+                      onClick={() =>
+                        router.push(`/lecturer/classlist?classId=${selectedClassId}&dateTime=${item.dateTime}`)
+                      }
+                    >
+                      <p className="text-sm md:text-xl text-gray-400">{formatDate(item.dateTime)}</p>
+                      <p
+                        className={`text-sm md:text-xl font-bold ${
+                          item.expectedAttendees - item.attendees.length > item.expectedAttendees / 2
+                            ? "text-red-500"
+                            : "text-green-500"
+                        }`}
+                      >
+                        {item.attendees.length}/{item.expectedAttendees}
+                      </p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+
+      {showCreateClass && <CreateClass hideCreateClass={hideCreateClass} />}
     </>
   );
 }
